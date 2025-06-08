c67f89b5f52bc7c5482e002da82d498152ff4917
Millie Houston <immaloser102@gmail.com>
2025-06-07 20:43:02 -0700

    app crashed? help

--- App/app.js
+++ App/app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const { PORT = 3001 } = process.env;
 
+mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
+
 app.listen(3001, () => {
   console.log(`Listening on ${PORT}`);
 });
