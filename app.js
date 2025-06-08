82ceee6627cab923337dd222f1944c78b042af05
Millie Houston <immaloser102@gmail.com>
2025-06-07 21:21:42 -0700

    Fixed the server issues

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require("express");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const { PORT = 3001 } = process.env;
 
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db');
+mongoose
+  .connect('mongodb://127.0.0.1:27017/wtwr_db')
+  .then(() => {
+    console.log("Connected to server");
+  })
+  .catch((e) => console.error(e));
 
-app.listen(3001, () => {
+
+
+  app.listen(3001, () => {
   console.log(`Listening on ${PORT}`);
 });
