201e4ca90b53119d0edb579dbf670d7e947294e2
Millie Houston <immaloser102@gmail.com>
2025-06-07 22:20:54 -0700

    Began working on the user.js and the clothing.js files

---
+++ models/clothingItems.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const clothingSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  weather: {
+    type: String,
+    required: true,
+  },
+  
+})
