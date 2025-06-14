4c028adcd1f57c57860fb94b2219c450f4a82cc7
Millie Houston <immaloser102@gmail.com>
2025-06-13 20:55:39 -0700

    Linked DB to PM and VSC... Want to rest, but cannot

--- app.js
+++ app.js
@@ -5,19 +5,20 @@ const clothingRouter = require("./routes/clothingitems");
 const mainRouter = require("./routes/index")
 const app = express();
 
-app.use(express.json());
+
 
 const { PORT = 3001 } = process.env;
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/wtwr_db')
   .then(() => {
-    console.log("Connected to server");
+    console.log("Connected to DB");
   })
   .catch((e) => console.error(e));
 
-
+app.use(express.json());
 app.use("/", mainRouter);
+
 
 app.listen(3001, () => {
   console.log(`Listening on ${PORT}`);
