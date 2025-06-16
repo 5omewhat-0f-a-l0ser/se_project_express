284b8baec2f27987c503bed1af969f58ba63d0be
Millie Houston <immaloser102@gmail.com>
2025-06-15 23:06:25 -0700

    Plowed through alot, considering my poor retention span atm

---
+++ controllers/likes.js
@@ -0,0 +1,3 @@
+const ClothingItem = require("../models/clothingItems");
+const Error400 = require("../utils/Error400");
+const Error404 = require("../utils/Error404");
---
+++ routes/likes.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+
+const {
+
+} = require("../controllers/likes");
+
+router.get("/", );
+router.delete("/:itemId", );
+
+module.exports = router;
