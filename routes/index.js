3fa1f1f8679f8c4ce70f5b6848f06617897e5419
Millie Houston <immaloser102@gmail.com>
2025-11-02 23:21:47 -0800

    Project 15 is going to make me want to dexit

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,23 @@
+const NotFoundError = require("../errors/Error404");
 const router = require("express").Router();
 
 const userRouter = require("./users");
 
 const clothingRouter = require("./clothingitems");
-const NotFounrError = require("../errors/Error404");
 
 const { login, createUser } = require("../controllers/users");
-const { userInfoBodyValidation, loginValidation } = require("../middlewares/validation");
-
+const {
+  userInfoBodyValidation,
+  loginValidation,
+} = require("../middlewares/validation");
 
 router.use("/users", userRouter);
 
 router.use("/items", clothingRouter);
 
-router.post("/signin",loginValidation, login);
-router.post("/signup",userInfoBodyValidation, createUser);
+router.post("/signin", loginValidation, login);
+router.post("/signup", userInfoBodyValidation, createUser);
 
-router.use((req, res, next) =>
-  next(new NotFoundError("Document not found"))
-);
+router.use((req, res, next) => next(new NotFoundError("Document not found")));
 
 module.exports = router;
